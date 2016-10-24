2cb318867c704897947a9b869d8c8d5813039ca4
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-10-23 20:27:37 -0500

    Added android package

---
+++ Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cs442.group10.compost_crossing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Compost-Crossing/app/src/test/java/com/cs442/group10/compost_crossing/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.cs442.group10.compost_crossing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
