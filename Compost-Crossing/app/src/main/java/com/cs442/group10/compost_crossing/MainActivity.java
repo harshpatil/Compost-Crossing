00c5b5c0b63060c4f3aa88e2a778ff9a11b5b244
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-10-30 14:51:58 -0500

    Implemented news article functionality

--- Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/MainActivity.java
+++ Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/MainActivity.java
@@ -1,13 +1,33 @@
 package com.cs442.group10.compost_crossing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cs442.group10.compost_crossing.newsArticle.Article;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button readArticle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readArticle = (Button) findViewById(R.id.readArticleButton);
+        readArticle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickingReadArticleButton();
+            }
+        });
+    }
+
+    public void onClickingReadArticleButton(){
+        Intent readArticleIntent = new Intent(this, Article.class);
+        startActivity(readArticleIntent);
     }
 }
