d7327df4bcf00bbd839300a307a14e11dc6949a5
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-10-30 17:42:24 -0500

    Removed home screen class

--- Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/MainActivity.java
+++ Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cs442.group10.compost_crossing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button Composter= (Button)findViewById(R.id.compostButton);
+        final Button residentButton= (Button)findViewById(R.id.residentButton);
+
+        Composter.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent i = new Intent(getBaseContext(), ViewListFragment.class);
+                startActivity(i);
+
+            }
+        });
+
+        residentButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+//                Intent i = new Intent(getBaseContext(), "give the required class name");
+                startActivity(i);
+
+            }
+        });
     }
 }
