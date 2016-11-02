47e9facf1ffaf578b91dfd46d89feff7cab6efba
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-11-01 20:53:42 -0500

    Removed title bar from article page

--- Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/newsArticle/Article.java
+++ Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/newsArticle/Article.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -37,6 +39,8 @@ public class Article extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.news_article);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
