f3c90526f7570f4e8a7c2060f1f200bfc7ede65c
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-11-02 21:42:58 -0500

    modified ui

--- Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/newsArticle/Article.java
+++ Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/newsArticle/Article.java
@@ -73,7 +73,6 @@ public class Article extends AppCompatActivity {
         });
 
         super.onCreate(savedInstanceState);
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.news_article);
 
