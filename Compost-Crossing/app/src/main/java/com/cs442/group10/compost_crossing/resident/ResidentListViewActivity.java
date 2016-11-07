c9c55a00c9c1e88949eed7c9b2032be4cdbe2e42
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-11-07 02:51:17 -0600

    added buttons for history and resident flow

--- Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/resident/ResidentListViewActivity.java
+++ Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/resident/ResidentListViewActivity.java
@@ -6,15 +6,36 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.cs442.group10.compost_crossing.R;
 
 public class ResidentListViewActivity extends AppCompatActivity {
 
+    Button history;
+    Button nearByComposter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resident_list_view);
+
+        history = (Button) findViewById(R.id.btnResidentHistory);
+        nearByComposter = (Button) findViewById(R.id.btnComposterNearBy);
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickHistoryButton();
+            }
+        });
+
+        nearByComposter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickNearByComposterButton();
+            }
+        });
     }
 
     public void back(View v){
@@ -23,5 +44,11 @@ public class ResidentListViewActivity extends AppCompatActivity {
 
     public void navigateToNewAdForm(View v){
         //navigating to posting new ad fragment goes here
+    }
+
+    public void onClickHistoryButton(){
+    }
+
+    public void onClickNearByComposterButton(){
     }
 }
